3ac0d93268aa1f1990b886f42e31739ca4e60746
Gabriel Teles <gabrielteleles@gmail.com>
2025-04-24 13:29:10 -0300

    fix: Add type declarations for image files

---
+++ src/vite-env.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="vite/client" />
+
+// Declarações de tipo para arquivos de imagem
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: string;
+  export default value;
+} 
